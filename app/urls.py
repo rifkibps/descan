626c1f57ad1a2c9bbe0aef00eeeff6609fcc0c0f
Rifki Gustiawan <rifkigst.bps@gmail.com>
2024-08-20 11:00:22 +0800

    Updating Project

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,9 @@ app_name = 'app'
 urlpatterns = [
     path('app/', views.DashboardClassView.as_view(), name="dashboard"),
     path('tabulasi/keluarga', views.TabulationsFamiliesClassView.as_view(), name="tab_families"),
+    path('tabulasi/keluarga/fetch-data', views.TabulationsFamiliesFetchClassView.as_view(), name="tab_families_fetch"),
+
+
     path('tabulasi/penduduk', views.TabulationsPopulationsClassView.as_view(), name="tab_populations"),
     path('manajemen-keluarga/', views.TabulationsFamiliesClassView.as_view(), name="mnj_families"),
     path('manajemen-penduduk/', views.TabulationsPopulationsClassView.as_view(), name="mnj_population"),
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models, helpers
+from django.http import JsonResponse
 from pprint import pprint
 
 
@@ -36,7 +37,27 @@ class TabulationsFamiliesClassView(LoginRequiredMixin, View):
             'home_ownership_state' : home_ownership_state
         }
         return render(request, 'app/tabulasi/tabulasi-keluarga.html', context)
-    
+
+class TabulationsFamiliesFetchClassView(LoginRequiredMixin, View):
+
+    def post(self, request):
+        print('Hello Wolrd')
+        is_ajax = request.headers.get('X-Requested-With') == 'XMLHttpRequest'
+        if is_ajax:
+            if request.method == 'POST':
+                print('Hello Wolrd')
+                # form = forms.BackendDataConsultForm(request.POST)
+
+                # if form.is_valid():
+                #     form.save()
+                #     return JsonResponse({"status": 'success'}, status=200)
+                # else:
+                #     return JsonResponse({"status": 'failed', "error": form.errors}, status=400)
+
+        return JsonResponse({'status': 'Invalid request'}, status=400)
+
+
+
 class TabulationsPopulationsClassView(LoginRequiredMixin, View):
     
     def get(self, request):
