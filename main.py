3ca0a2fab8a8aa875d3416876b75beab3aa84d84
rifkibps <rifkigst.bps@gmail.com>
2024-09-18 15:24:43 +0800

    add starter app

---
+++ main.py
@@ -0,0 +1,9 @@
+import webbrowser 
+  
+# then make a url variable 
+url = "http://192.168.1.81:8000"
+  
+# then call the get method to select the code  
+# for new browser and call open method  
+# described above 
+webbrowser.open(url) 
